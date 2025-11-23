a3b3bc6c4a8b7b233a804512cad99e6e87af8e88
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-11-24 03:41:00 +0900

    fix

--- main.rb
+++ main.rb
@@ -477,7 +477,7 @@ class NostrRelay
       ds = ds.where(kind: f['kinds']) if f['kinds']
       ds = ds.where{created_at >= f['since']} if f['since']
       ds = ds.where{created_at <= f['until']} if f['until']
-      ds = ds.where{Sequel.like(:content, escape_like(f['search'])) if f['search']
+      ds = ds.where{Sequel.like(:content, escape_like(f['search']))} if f['search']
       
       # NIP-12: Generic tag queries (#e, #p, etc)
       f.each do |key, values|
