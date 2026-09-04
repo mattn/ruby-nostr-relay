ac2abcd5b47d519e4c190c1add303e76e1568c3a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-09-04 17:17:07 +0900

    Align NIP-11 with relay-tagged NIPs

--- main.rb
+++ main.rb
@@ -675,7 +675,7 @@ class RelayInfo
         icon: ENV['RELAY_ICON'] || "",
         relay_countries: (ENV['RELAY_COUNTRIES'] || "JP").split(',').map(&:strip).reject(&:empty?),
         # Updated supported_nips based on common implementations and NIPs handled
-        supported_nips: [1, 2, 4, 9, 11, 12, 15, 16, 20, 22, 26, 28, 33, 40, 50, 62, 70],
+        supported_nips: [1, 4, 9, 11, 26, 40, 50, 62, 70],
         software: "https://github.com/mattn/ruby-nostr-relay",
         version: "1.0.0",
         limitation: {
